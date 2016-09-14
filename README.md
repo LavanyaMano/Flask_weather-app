c2e58d909d87b6bc3f722d246a26fabb3c3879ac
lava m <lavanymkanna@gmail.com>
2016-09-14 10:34:25 -0400

    forms added

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Flask app to get current weather for a city and the forecast.
 ## Setup
 * `mkvirtualenv -p python3 flask-package`
 * `pip install -r requirements.txt`
+*Copy settings-dist.py to settings.py and add your openweathermap API key
+
 
 ## Run the flask app
 `python run.py`
--- app/views.py
+++ app/views.py
@@ -5,16 +5,17 @@ from app.forms import WeatherForm
 
 @app.route("/")
 def index():
-
-
     return render_template("index.html")
 
 @app.route("/current",methods=["GET","POST"])
 def current_weather():
     weather_form = WeatherForm(request.form)
+    if request.method == "POST" and weather_form.validate():
+        
+        pass
     return render_template("current.html",weather_form=weather_form)
 
 @app.route("/forecast",methods=["GET","POST"])
 def forecast_weather():
 
-    return render_template("forecast.html")
+    return render_template("forecast.html",weather_form=weather_form)
