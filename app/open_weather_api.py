0125a2ba10407961534fb00ec0dfe1340e897aae
lava m <lavanymkanna@gmail.com>
2016-09-15 13:10:07 -0400

    16 day forecast added

---
+++ app/open_weather_api.py
@@ -0,0 +1,107 @@
+import requests
+
+class OpenWeatherAPI():
+    def __init__(self, api_key, units="imperial"):
+        self.base_url = "http://api.openweathermap.org/data/2.5"
+        self.base_payload = {
+            "appid": api_key,
+            "units": units,
+        }
+
+    def get_payload(self, **kwargs):
+        payload = {}
+
+        for key, value in self.base_payload.items():
+            payload[key] = value
+
+        for key, value in kwargs.items():
+            payload[key] = value
+
+        return payload
+
+
+    def get_current_weather(self, city, country_code=""):
+        q = ""
+
+        if country_code:
+            q = "{},{}".format(city, country_code)
+        else:
+            q = city
+
+        payload = self.get_payload(q=q)
+        url = "{}{}".format(self.base_url, "/weather")
+        r = requests.get(url, params=payload)
+        result = result_or_error(r.json())
+
+        return DailyWeather(
+                description = result['weather'][0]['description'],
+                icon = result['weather'][0]['icon'],
+                temp = result['main']['temp'],
+                dt = result['dt'],
+                wind = result['wind']['speed'],
+                pressure = result['main']['pressure'],
+                humidity = result['main']['humidity'],
+                sunrise = result['sys']['sunrise'],
+                sunset = result['sys']['sunset']
+            )
+    def get_daily_weather(self, city, country_code="", num_days=16):
+        q = ""
+
+        if country_code:
+            q = "{},{}".format(city, country_code)
+        else:
+            q = city
+
+        payload = self.get_payload(q=q, cnt=num_days)
+
+        url = "{}{}".format(self.base_url, "/forecast/daily")
+        r = requests.get(url, params=payload)
+        result = result_or_error(r.json())
+
+        weather_list =[]
+        for item in result["list"]:
+            daily_weather = DailyWeather(
+                icon = item["weather"][0]["icon"],
+                description = item["weather"][0]["description"],
+                temp = item["temp"]["day"],
+                temp_night = item["temp"]["night"],
+                dt = item["dt"],
+                pressure = item["pressure"],
+                humidity = item["humidity"],
+            )
+            weather_list.append(daily_weather)
+
+        return weather_list
+
+class DailyWeather():
+    def __init__(self,
+        description="",
+        icon=None,
+        dt=None,
+        temp=None,
+        temp_night=None,
+        wind=None,
+        pressure=None,
+        humidity=None,
+        sunrise=None,
+        sunset=None):
+
+        self.description = description
+        self.icon = icon
+        self.dt = dt
+        self.temp = temp
+        self.temp_night = temp_night
+        self.wind = wind
+        self.pressure = pressure
+        self.humidity = humidity
+        self.sunrise = sunrise
+        self.sunset = sunset
+
+    def __str__(self):
+        return "Description: {}, Temp: {}".format(self.description, self.temp)
+
+def result_or_error(result):
+    if str(result.get("cod")) != "200":
+        raise ValueError("City not found")
+
+    return result
--- app/views.py
+++ app/views.py
@@ -1,21 +1,45 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 
 from app import app
 from app.forms import WeatherForm
+from app.open_weather_api import OpenWeatherAPI
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/current",methods=["GET","POST"])
+@app.route("/current", methods=["GET", "POST"])
 def current_weather():
     weather_form = WeatherForm(request.form)
+    weather_item = None
+
     if request.method == "POST" and weather_form.validate():
-        
-        pass
-    return render_template("current.html",weather_form=weather_form)
+        city = weather_form.city.data
+        country_code = weather_form.country_code.data
 
-@app.route("/forecast",methods=["GET","POST"])
+        try:
+            api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"], weather_form.units.data)
+            weather_item = api.get_current_weather(city, country_code)
+        except ValueError as e:
+            flash(str(e), "warning")
+
+    return render_template("current.html",
+        weather_form=weather_form, weather_item=weather_item)
+
+@app.route("/forecast", methods=["GET", "POST"])
 def forecast_weather():
+    weather_form = WeatherForm(request.form)
+    weather_list = None
 
-    return render_template("forecast.html",weather_form=weather_form)
+    if request.method == "POST" and weather_form.validate():
+        city = weather_form.city.data
+        country_code = weather_form.country_code.data
+
+        try:
+            api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"], weather_form.units.data)
+            weather_list = api.get_daily_weather(city, country_code)
+        except ValueError as e:
+            flash(str(e), "warning")
+    return render_template("forecast.html",
+        weather_form=weather_form,
+        weather_list=weather_list)
